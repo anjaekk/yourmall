fdbaca2f149982df8a870251a203d581c8242206
anjaekk <anjaekk@gmail.com>
2021-05-27 01:41:58 +0900

    메뉴에 URL 지정

--- clothes/views.py
+++ clothes/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+
 def index(request):
-    return render(request, 'clothes/index.html')
+    today = datetime.today().date()
+    context = {"date":today}
+    return render(request, 'clothes/index.html', context=context)
+
+def trend_detail(request, trend):
+    context = {"name":trend}
+    return render(request, 'clothes/detail.html', context=context)
