46bc40fadc0c5f40844a60b71ce33273c5bfa25e
anjaekk <anjaekk@gmail.com>
2021-05-28 16:24:18 +0900

    예외 페이지 404에러 처리

--- clothes/views.py
+++ clothes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 
@@ -10,5 +11,11 @@ def index(request):
     return render(request, 'clothes/index.html', context=context)
 
 def trend_detail(request, trend):
-    context = {"name":trend}
+    context = dict()
+    if trend == "2021ss" :
+        context["name"] = "S/S 2021"
+        context["description"] = "2021년 Spring/Summer 트렌드"
+        context["img_path"] = "clothes/images/2021ss.jpg"
+    else:
+        raise Http404()
     return render(request, 'clothes/detail.html', context=context)
