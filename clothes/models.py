c5104f729c484c7919d8871bc75eabda2d7ecf7e
anjaekk <anjaekk@gmail.com>
2021-05-28 20:29:59 +0900

    마이그레이션 생성 및 데이터베이스 반영

--- clothes/models.py
+++ clothes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    img_path = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
