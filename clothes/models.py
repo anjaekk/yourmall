d03d8e000aa08e0944182da86e360390d9cc7c78
anjaekk <anjaekk@gmail.com>
2021-05-29 15:02:06 +0900

    패션 트렌드 데이터 추가

--- clothes/models.py
+++ clothes/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.model):
+class Trend(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    img_path = models.CharField(max_length=250)
+    img_path = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
